1ef6ac723ffa37a86678cbc41d09ce870bbf8be2
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2022-05-24 10:55:47 -0400

    setup redis cliemt connection to handle user and password

--- src/config/redis-client.js
+++ src/config/redis-client.js
@@ -1,6 +1,19 @@
 import { createClient } from "redis";
+import { SERVICE } from "./constants.js";
 
-const url = process.env.REDIS_URL;
+let url;
+const REDIS_HOST = process.env["REDIS_HOST"];
+const REDIS_PORT = process.env["REDIS_PORT"] || 6379;
+const REDIS_USER = process.env["REDIS_USER"];
+const REDIS_PW = process.env["REDIS_PW"];
+if (REDIS_HOST) {
+  url = "redis://";
+  if (REDIS_USER && REDIS_PW) {
+    url += `${REDIS_USER}:${REDIS_PW}@`;
+  }
+  url += `${REDIS_HOST}:${REDIS_PORT}`;
+}
+console.log("LOG:  ~ file: redis-client.js ~ line 15 ~ url", url);
 
-export const client = createClient({ url });
+export const client = createClient({ url, name: SERVICE });
 await client.connect();
