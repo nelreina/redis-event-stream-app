428c76314373606121542e54dac41391c516bcc5
Nelreina <nelreina@gmail.com>
2022-08-24 11:39:23 +0000

    spread stream data

--- src/event-handler.js
+++ src/event-handler.js
@@ -1,5 +1,7 @@
 import logger from "./config/logger.js";
 
-export const handler = async (msg) => {
-  logger.info(JSON.stringify(msg));
+export const handler = async (stream) => {
+  const { streamId, aggregateId, event, payload } = stream;
+
+  logger.info(JSON.stringify({ event, aggregateId, payload }));
 };
