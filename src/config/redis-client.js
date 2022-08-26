75f6de5a074a757aba0a3def6180001efca84c0b
Nelreina <nelreina@gmail.com>
2022-08-26 19:15:09 +0000

    added addToStream to template

--- src/config/redis-client.js
+++ src/config/redis-client.js
@@ -1,11 +1,14 @@
 import { createClient } from "redis";
 import { SERVICE } from "./constants.js";
+import { addToEventLog } from "@nelreina/redis-stream-consumer";
 
 let url;
 const REDIS_HOST = process.env["REDIS_HOST"];
 const REDIS_PORT = process.env["REDIS_PORT"] || 6379;
 const REDIS_USER = process.env["REDIS_USER"];
 const REDIS_PW = process.env["REDIS_PW"];
+const STREAM = process.env["STREAM"];
+
 if (REDIS_HOST) {
   url = "redis://";
   if (REDIS_USER && REDIS_PW) {
@@ -15,4 +18,14 @@ if (REDIS_HOST) {
 }
 
 export const client = createClient({ url, name: SERVICE });
-await client.connect();
+
+export const addToStream = async (event, declarationId, payload) => {
+  const streamData = {
+    streamKeyName: STREAM,
+    aggregateId: declarationId,
+    payload,
+    event,
+    serviceName: SERVICE,
+  };
+  await addToEventLog(client, streamData);
+};
