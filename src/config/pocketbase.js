134ed69ba4aa552e89c2d46e0c80f6891c443b65
Nelreina <nelreina@gmail.com>
2022-12-06 08:54:30 -0400

    connect in index.js

--- src/config/pocketbase.js
+++ src/config/pocketbase.js
@@ -10,14 +10,3 @@ const POCKETBASE_PASSWORD = process.env["POCKETBASE_PASSWORD"];
 
 global.EventSource = EventSource;
 export const pbAdmin = new PocketBase(POCKETBASE);
-(async () => {
-  try {
-    const authData = await pbAdmin.admins.authWithPassword(
-      POCKETBASE_ADMIN,
-      POCKETBASE_PASSWORD
-    );
-    logger.info("PocketBase admin authenticated: " + JSON.stringify(authData));
-  } catch (error) {
-    logger.error("PocketBase admin authentication failed", error.message);
-  }
-})();
