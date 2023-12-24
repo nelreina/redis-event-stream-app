65e3adfa329f4e7b5a655a99b35f2e9d562fe882
Nelson Reina (@ibis-nreina) <nelson.reina@ibis-management.com>
2023-12-24 04:07:58 -0400

    main: upgraded @nelreina/redis-stream-consumer

--- src/event-handler.js
+++ src/event-handler.js
@@ -1,8 +1,8 @@
 import logger from "./config/logger.js";
 
 export const handler = async (stream) => {
-  const { streamId, aggregateId, event, payload } = stream;
+  const { streamId, aggregateId, event, payload, serviceName } = stream;
 
-  logger.info(JSON.stringify({ event, aggregateId, payload }));
+  logger.info(JSON.stringify({ serviceName, event, aggregateId, payload }));
   stream.ack(streamId);
 };
